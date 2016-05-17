b942b24fefc3a5fe7a5d077fe2391ccbb2058524
David Ramsay <dramsay@mit.edu>
2016-05-17 13:54:06 -0400

    change to warn level logging

--- globalConfig.py
+++ globalConfig.py
@@ -3,7 +3,7 @@ import logging
 
 
 #set log output to screen and level to DEBUG level
-logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+logging.basicConfig(stream=sys.stderr, level=logging.WARN)
 
 #create one shared instance of logging
 log = logging.getLogger()
