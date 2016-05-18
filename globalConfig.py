baea66e4da9dc6d73eb27955aac0fe7851421521
David Ramsay <dramsay@mit.edu>
2016-05-18 14:58:43 -0400

    change logger

--- globalConfig.py
+++ globalConfig.py
@@ -2,9 +2,16 @@ import sys
 import logging
 
 
+level = logging.INFO
+
 #set log output to screen and level to DEBUG level
-logging.basicConfig(stream=sys.stderr, level=logging.WARN)
+logging.basicConfig(stream=sys.stderr)
 
 #create one shared instance of logging
-log = logging.getLogger()
+log = logging.getLogger(__name__)
+log.setLevel(level)
+log.propagate = 0
 
+ch = logging.StreamHandler()
+ch.setLevel(level)
+log.addHandler(ch)
